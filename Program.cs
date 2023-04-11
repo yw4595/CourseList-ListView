394fb3429fcd56f28f05b5a1fa1225cfdc184ab5
yw4595 <106081987+yw4595@users.noreply.github.com>
2023-04-11 16:58:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+/*
+ * Author: Yanzhi Wang
+ * Purpose: This class provides functionality for managing courses and their reviews.
+ * Restrictions: This class is designed for use in a Windows Forms application and may not work properly in other contexts.
+ */
 
 namespace PE19
 {
